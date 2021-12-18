00577189fa1c65e1e8008ab40652d98524bdafd2
Yosmar Lobo <yosmarlobo34@gmail.com>
2021-12-18 14:18:43 -0500

    sabado

---
+++ sabado/index.js
@@ -0,0 +1,14 @@
+const sabado = (array) => {
+  let suma = 0;
+  const menor = array[0] < array[1] ? array[0] : array[1];
+  const mayor = array[0] > array[1] ? array[0] : array[1];
+  let sumaArray = array[0] + array[1];
+  {
+    suma += i;
+  }
+  const resultado = {
+    sumaArray: sumaArray,
+    sumaIntermedia: suma,
+  };
+  return resultado;
+};
